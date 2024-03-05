8fb9e4b797aad4338123232add01395d0c42bb5a
Chanheung-Cho <125518566+Chanheung-Cho@users.noreply.github.com>
2024-03-04 23:12:24 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,7 +47,7 @@
 			$search_input = $search.find('input');
 
 		$body
-			.on('click', '[href="#search"]', function(event) {
+			.on('click', '[href="#search"]{text-decoration: none;}', function(event) {
 
 				event.preventDefault();
 
